98330e0dfea1d2476f1ca0ece4673d03a23765b8
Kartik Arora <90651659+KartikAroraOfficial@users.noreply.github.com>
2022-11-10 01:15:42 +0630

    Add files via upload

--- led_controller.py
+++ led_controller.py
@@ -1,26 +1,20 @@
 # This Python file uses the following encoding: utf-8
+
 from gpiozero import LED
-import RPi.GPIO as GPIO
 import time
-from morse import DOT_TIME, DASH_TIME
+import morse
 
 
-
-class led_controller:
-
-    led = LED(14)
-
+class led_controller(object):
     def __init__(self):
-        GPIO.setmode(GPIO.BCM)
-
+        self.led = LED(14)
 
     def blinkDit(self):
         self.led.on()
-        time.sleep(DOT_TIME)
+        time.sleep(morse.DOT_TIME)
         self.led.off()
-
 
     def blinkDah(self):
         self.led.on()
-        time.sleep(DASH_TIME)
+        time.sleep(morse.DASH_TIME)
         self.led.off()
--- morse.py
+++ morse.py
@@ -1,10 +1,8 @@
-# This Python file uses the following encoding: utf-8
-from msilib.schema import Error
 from led_controller import led_controller
 import time
 
 
-UNIT = 0.3                 # unit time 
+UNIT = 0.3                 # unit time
 DOT_TIME = 1*UNIT          # also the time for space between the symbols - 1 unit
 DASH_TIME = 3*UNIT         # also the space between letters - 3 units
 WORD_SPACE = 7*UNIT        # space between two words - 7 units
@@ -12,46 +10,48 @@ SAME_LETTER_TIME = UNIT    # 1 unit
 DIFF_LETTER_TIME = 3*UNIT  # 3 units
 
 morse_dict = {
-    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..',
-    'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
-    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..', '1': '.----',
-    '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
-    '0': '-----', ' ': '/'
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
+    '9': '----.', '0': '-----', ' ': '/'
 }
 
+controller = led_controller()
 
 class morse:
     def __init__(self):
         super().__init__()
-        self.controller = led_controller.led_cotnroller()
+
 
     def translate(self, phrase):
         result = ''
         for x in phrase:
             if (x.upper() in morse_dict.keys()):
-                result += morse_dict[x]
+                result += morse_dict[x.upper()] + " "
             else:
-                raise LookupError("Invalid Character in String!")
-                # result = 'Error: Invalid character in string!'
-                # return result
+                result = 'Error: Invalid character in string!'
+                return result                                       # returns the error message string!
         return result
 
+# character received is always in upper()
     def blink_letter(self, character):
-        if (character.upper() in morse_dict.keys()):
-            for x in range(len(morse_dict[character])):
-                if morse_dict[x] == '.':
-                    self.controller.blinkDit()
-                    if x == len(morse_dict[x])-1:
+        if (character in morse_dict.keys()):
+            for x in range(len( morse_dict[character])):
+                if morse_dict[character][x] == '.':                 # checks if the character in the value received is a Dot
+
+                    controller.blinkDit()
+                    if x == len(morse_dict[character])-1:
                         time.sleep(DIFF_LETTER_TIME)
                     else:
                         time.sleep(SAME_LETTER_TIME)
-                elif morse_dict[x] == '-':
-                    self.controller.blinkDah()
-                    if x == len(morse_dict[x])-1:
+                elif morse_dict[character.upper()][x] == '-':       # checks if the character in the value received is a Dash
+                    controller.blinkDah()
+                    if x == len(morse_dict[character])-1:
                         time.sleep(DIFF_LETTER_TIME)
-                    else:
+                    else:                                           # checks if the character in the value received is a /
                         time.sleep(SAME_LETTER_TIME)
                 else:
                     time.sleep(WORD_SPACE)
-        else:
-            raise LookupError
